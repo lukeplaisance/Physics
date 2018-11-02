a1db22a2378cd5eff66ff6ea339f0971a70d34ee
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2018-11-02 15:33:35 -0500

    created particle behaviour script

---
+++ Assets/Scripts/LukeTools/ParticleBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LukeTools
+{
+    public class ParticleBehaviour : MonoBehaviour
+    {
+        IMoveable moveable;
+        public Transform t;
+        public ParticleData pd;
+       
+        // Use this for initialization
+        void Start()
+        {
+            moveable = new LinearMove(pd);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            moveable.Move(t);
+        }
+    }
+}
