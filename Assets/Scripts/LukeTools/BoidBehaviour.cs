e7d00da98a746498d796c8aa4ca1cd24647151d5
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2018-11-07 15:00:44 -0600

    Dispersion and Allignment methods
    worked on the Dispersion and allignment methods for boids

--- Assets/Scripts/LukeTools/BoidBehaviour.cs
+++ Assets/Scripts/LukeTools/BoidBehaviour.cs
@@ -20,7 +20,7 @@ namespace LukeTools
             {
                 v1 = Boid_Cohesion(b);
                 v2 = Boid_Dispersion(b);
-                v2 = Boid_Allignment(b);
+                v3 = Boid_Allignment(b);
 
                 b.Velocity = b.Velocity + v1 + v2 + v3;
                 b.Position = b.Position + b.Velocity;
@@ -29,20 +29,60 @@ namespace LukeTools
 
         public Vector3 Boid_Cohesion(ParticleData b)
         {
+            //the number of boids
             var N = Boids.Count;
 
+            //the perceived center
             Vector3 pc = new Vector3(0,0,0);
 
+            //finds the average position of each boid
             foreach (var item in Boids)
             {
-                if(item != b)
-                    if((item.Position - b.Position).magnitude <= 1)
-                    {
-                        pc += item.Position;
-                    }
+                if (item != b)
+                {
+                    pc += item.Position;
+                }   
             }
             pc = pc / (N - 1);
             return (pc - b.Position) / 100;
+        }
+
+        public Vector3 Boid_Dispersion(ParticleData b)
+        {
+            //the displacement of each boid
+            Vector3 c = new Vector3(0, 0, 0);
+
+            //if the position of a boid is less thatn 1 units away from another, 
+            //the boid will go the opposite way
+            foreach (var item in Boids)
+            {
+                if(item != b)
+                    if ((item.Position - b.Position).magnitude <= 1)
+                    {
+                        c = c - (item.Position - b.Position);
+                    }
+            }
+            return c;
+        }
+
+        public Vector3 Boid_Allignment(ParticleData b)
+        {
+            //the number of boids
+            var N = Boids.Count;
+
+            //percieved velocity
+            Vector3 pv = new Vector3(0,0,0);
+
+            //finds the average velocity of each boid
+            foreach(var item in Boids)
+            {
+                if(item != b)
+                {
+                    pv += b.Velocity;
+                }
+            }
+            pv = pv / (N - 1);
+            return (pv - b.Velocity) / 8;
         }
     }
 }
