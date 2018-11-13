d9cf6556b18e373989ef0a0f34a9467bec1c7a95
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2018-11-13 12:51:03 -0600

    created AABB scene

---
+++ Assets/Scripts/AABB/CollisionBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AABB
+{
+    public class CollisionBehaviour : MonoBehaviour
+    {
+        public CollisionVolume2D cv;
+
+        private List<Vector2> xValues;
+        private List<Vector2> yValuse;
+        public List<CollisionVolume2D> activeList;
+        public List<CollisionVolume2D> closedList;
+
+        public void Empty()
+        {
+            
+        }
+
+        //adds a point to the AABB if necessary to contain the shape
+        public void Add(Vector3 vec)
+        {
+            if (vec.x < cv.min.x)
+                cv.min.x = vec.x;
+
+            if (vec.y < cv.min.y)
+                cv.min.y = vec.y;
+        }
+    }
+}
---
+++ Assets/Scripts/AABB/CollisionVolume2D.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AABB
+{
+    public class CollisionVolume2D
+    {
+        public Vector2 min;
+        public Vector2 max;
+        public bool isColliding = false;
+    }
+}
