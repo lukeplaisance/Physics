1adefbd8ada1a76b72503ef3ec5386c30afe6348
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2018-11-13 16:47:12 -0600

    working on collision detection

---
+++ Assets/Scripts/AABB/SortAndSweepBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace AABB
+{
+    public class SortAndSweepBehaviour : MonoBehaviour
+    {
+        public List<CollisionVolume2D> xValues;
+        private List<CollisionVolume2D> yValues;
+        public List<CollisionVolume2D> activeList;
+        public List<CollisionVolume2D> closedList;
+
+        // Use this for initialization
+        void Start()
+        {
+            xValues = new List<CollisionVolume2D>(GetComponents<CollisionVolume2D>());
+            yValues = new List<CollisionVolume2D>(GetComponents<CollisionVolume2D>());
+        }
+
+        public void CheckCollisionForXAxis()
+        {
+            xValues.OrderBy(v => v.min);
+            for(int i = 0; i < xValues.Count; i++)
+            {
+                foreach(var volume in xValues)
+                {
+                    activeList.Add(volume);
+                    if(activeList.Count >= 2)
+                    {
+                        if(activeList[0].min.x < activeList[1].max.x)
+                        {
+                            closedList.Add(activeList[0]);
+                            activeList.Remove(activeList[0]);
+                            Debug.Log("collision on X axis");
+                        }
+                    }
+                }
+            }
+        }
+
+        public void CheckCollisionForYAxis()
+        {
+            yValues.OrderBy(v => v.min);
+            for (int i = 0; i < yValues.Count; i++)
+            {
+                foreach (var volume in yValues)
+                {
+                    activeList.Add(volume);
+                    if (activeList.Count >= 2)
+                    {
+                        if (activeList[0].min.y < activeList[1].max.y)
+                        {
+                            closedList.Add(activeList[0]);
+                            activeList.Remove(activeList[0]);
+                            Debug.Log("collision on Y axis");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
